947e76feb66d393afabbcc0b97aa7f19709a5261
kartmos <kartemgithub@gmail.com>
2025-07-25 00:49:22 +0300

    test new  feature with massage_threads_id

--- configs.go
+++ configs.go
@@ -279,6 +279,7 @@ func (chat *BaseChat) params() (Params, error) {
 
 	params.AddFirstValid("chat_id", chat.ChatID, chat.ChannelUsername)
 	params.AddNonZero("reply_to_message_id", chat.ReplyToMessageID)
+	params.AddNonZero("message_thread_id", chat.MessageThreadID)
 	params.AddBool("disable_notification", chat.DisableNotification)
 	params.AddBool("allow_sending_without_reply", chat.AllowSendingWithoutReply)
 	params.AddBool("protect_content", chat.ProtectContent)
